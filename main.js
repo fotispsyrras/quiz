05e618a6eb7eef2829805208efab93139bd80d78
fotis-psyrras <fotis-psyrras@users.noreply.github.com>
2020-06-21 00:00:18 +0300

    styl

--- main.js
+++ main.js
@@ -4,25 +4,32 @@ let erotisi =document.createElement('p')
 erotisi.innerHTML
 let keli=document.createElement('td')
 
-const app = document.querySelector('#app')
+//dimiourgia enos div me id=app
+app.setAttribute("id","app")
+
+//dimiourgia toy QUIZ time
 const header =document.createElement('h1')
 header.innerText="QUIZ time"
 header.style.color = 'rgb(42, 42, 172)'
 app.append(header)
 
-index=0
+let index=0
 
 function nextQuestion(questions){
-    let erotisi = document.createElement('p')
+    
+    let erotisi = document.createElement('h2')
+    erotisi.innerHTML=''
     erotisi.innerHTML=questions[index].question
+    app.append(erotisi)
+    
+    
 
 }
-
-
 
 fetch('questions.json')
 .then(res => res.json())
 .then(questions => { 
+    console.log(questions)
     nextQuestion(questions)
     
 })
