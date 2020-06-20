56a7cfc324521997b8adfd142faf2e482c0cfe0b
fotis-psyrras <fotis-psyrras@users.noreply.github.com>
2020-06-20 22:19:22 +0300

    recursive function +fetch+header

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
 "use strict"
+
+let erotisi =document.createElement('p')
+erotisi.innerHTML
+let keli=document.createElement('td')
+
+const app = document.querySelector('#app')
+const header =document.createElement('h1')
+header.innerText="QUIZ time"
+header.style.color = 'rgb(42, 42, 172)'
+app.append(header)
+
+index=0
+
+function nextQuestion(questions){
+    let erotisi = document.createElement('p')
+    erotisi.innerHTML=questions[index].question
+
+}
+
+
+
+fetch('questions.json')
+.then(res => res.json())
+.then(questions => { 
+    nextQuestion(questions)
+    
+})
+.catch(err => console.log(err))
